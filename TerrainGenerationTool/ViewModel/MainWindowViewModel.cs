9b8b98f30ddd8be05ea8cd2585f43f0a355f7b50
StuartFlannigan <stuart.flannigan@gmail.com>
2026-02-17 01:25:01 +0000

    Setup base MVVM architecture with ViewModelBase class

---
+++ TerrainGenerationTool/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TerrainGenerationTool.MVVM;
+
+namespace TerrainGenerationTool.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
