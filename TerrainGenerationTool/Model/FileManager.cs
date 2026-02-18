baede9b4d46c657b24c55db76b5123f18da185ff
StuartFlannigan <stuart.flannigan@gmail.com>
2026-02-18 20:54:01 +0000

    Load and display a bitmap image

---
+++ TerrainGenerationTool/Model/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TerrainGenerationTool.Model
+{
+    internal class FileManager
+    {
+        public string SelectFile(string title = "Select File", string filter = "All files (*.*)|*.*")
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = filter,
+                Multiselect = false,
+                Title = title
+            };
+            bool? result = fileDialog.ShowDialog();
+
+            if (result == true)
+                return fileDialog.FileName;
+            else
+                return String.Empty;
+        }
+
+        public void LoadFile(string filePath)
+        {
+
+        }
+    }
+}
--- TerrainGenerationTool/ViewModel/MainWindowViewModel.cs
+++ TerrainGenerationTool/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 using TerrainGenerationTool.MVVM;
+using TerrainGenerationTool.Model;
+using System.Drawing;
+using System.IO;
 
 namespace TerrainGenerationTool.ViewModel
 {
     class MainWindowViewModel : ViewModelBase
     {
+        private RelayCommand? selectCommand;
+        public RelayCommand SelectCommand => selectCommand??= new RelayCommand(execute => SelectFile());
 
+        private string? filePath;
+        public string FilePath { get => filePath??= defaultFilePathText;  set { filePath = value; OnPropertyChanged(nameof(FilePath)); } }
+
+        private string defaultFilePathText;
+
+        public MainWindowViewModel()
+        {
+            defaultFilePathText = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "TestMap.bmp");
+
+            Debug.WriteLine(defaultFilePathText);
+        }
+
+        private void SelectFile()
+        {
+            Debug.WriteLine("Selecting File");
+            FileManager fileManager = new FileManager();
+            string path;
+            FilePath = (!string.IsNullOrEmpty(path = fileManager.SelectFile("Select Image", "Bitmap files (*.bmp)|*.bmp")) ? path : FilePath);
+            Debug.Write(FilePath);
+
+            
+        }
     }
 }
